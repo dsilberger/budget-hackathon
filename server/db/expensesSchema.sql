214bace9b7879b5e2e533ab5e80f9300c8282499
Joe Yan <chengmu.yan@gmail.com>
2020-03-10 15:24:08 -0400

    Create initial expenses and categories schema with sample data

---
+++ server/db/expensesSchema.sql
@@ -0,0 +1,33 @@
+create database if not exists budget;
+
+use budget;
+
+create table if not exists Categories (
+  id integer auto_increment primary key,
+  name varchar(255) not null
+);
+
+create table if not exists Expenses (
+  id integer auto_increment primary key,
+  date date not null,
+  description varchar(255),
+  categoryId integer not null,
+  accountName varchar(255),
+  amount100 integer not null,
+
+  constraint foreign key (categoryId) references Categories (id)
+);
+
+truncate table Categories;
+insert into Categories (name) values ("Groceries");
+insert into Categories (name) values ("Restaurants");
+insert into Categories (name) values ("Mortgage & Rent");
+
+truncate table Expenses;
+insert into Expenses (date, description, categoryId, accountName, amount100)
+values
+  ("2019-01-01", "Skyba", 1, "Credit Card 1", 4900),
+  ("2019-01-01", "Feedfish", 1, "Credit Card 1", 6080),
+  ("2019-01-01", "Vinte", 2, "Credit Card 1", 1012),
+  ("2019-01-01", "Buzzshare", 2, "Credit Card 1", 115000),
+  ("2019-01-01", "Avaveo", 3, "Credit Card 1", 30300);
