3f1285db57520c1c6fe143b529dfdb101b4d5fd2
Joe Yan <chengmu.yan@gmail.com>
2020-03-10 16:50:15 -0400

    Create ExpensesModel.getCategories()

--- server/models/expensesModel.js
+++ server/models/expensesModel.js
@@ -4,29 +4,40 @@ const model = {
   addCategory: ({ name }) => {
     const q = `
       insert into Categories (name)
-      values (?)
-    `;
+      values (?)`;
 
     return db.queryAsync(q, [name]).then(({ insertId }) => model._getCategoryById(insertId));
+  },
+
+  getCategories: () => {
+    const q = `
+      select * from Categories`;
+
+    return db.queryAsync(q);
   },
 
   _getCategoryById: id => {
     const q = `
       select * from Categories c
-      where c.id = ?
-    `;
+      where c.id = ?`;
 
     return db.queryAsync(q, [id]).then(dbRes => dbRes[0]);
   }
+
+  // addExpense: ({date, description, category, accountName, amount100}) => {
+  //   const q = `
+
+  //   `
+  // }
 };
 
 module.exports = model;
 
-module.exports
-  .addCategory({ name: "Test 2" })
-  .then(dbRes => console.log(dbRes))
-  .catch(err => {
-    console.error(err);
-  });
+// module.exports
+//   .addCategory({ name: "Test 2" })
+//   .then(dbRes => console.log(dbRes))
+//   .catch(err => {
+//     console.error(err);
+//   });
 
-// module.exports.getCategory(1).then(dbRes => console.log(dbRes));
+module.exports.getCategories().then(dbRes => console.log(dbRes));
