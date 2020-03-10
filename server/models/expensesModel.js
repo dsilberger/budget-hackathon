bed655ff78114eee25e2e871c83330851cd5c36f
Joe Yan <chengmu.yan@gmail.com>
2020-03-10 17:07:02 -0400

    Create ExpensesModel.addExpense()

--- server/models/expensesModel.js
+++ server/models/expensesModel.js
@@ -22,13 +22,21 @@ const model = {
       where c.id = ?`;
 
     return db.queryAsync(q, [id]).then(dbRes => dbRes[0]);
+  },
+
+  addExpense: ({ date, description, category: categoryName, accountName, amount100 }) => {
+    const q = `
+      insert into Expenses (date, description, categoryId, accountName, amount100)
+      values (?, ?, ?, ?, ?)`;
+
+    return model
+      .getCategories()
+      .then(categories => {
+        let categoryId = categories.find(e => e.name === categoryName).id;
+        return categoryId ? categoryId : model.addCategory({ name: categoryName }).then(({ id }) => id);
+      })
+      .then(categoryId => db.queryAsync(q, [date, description, categoryId, accountName, amount100]));
   }
-
-  // addExpense: ({date, description, category, accountName, amount100}) => {
-  //   const q = `
-
-  //   `
-  // }
 };
 
 module.exports = model;
@@ -40,4 +48,13 @@ module.exports = model;
 //     console.error(err);
 //   });
 
-module.exports.getCategories().then(dbRes => console.log(dbRes));
+// model
+//   .addExpense({
+//     date: "2020-02-02",
+//     description: "testing addExpense()",
+//     category: "Mortgage & Rent",
+//     accountName: "Test Acct",
+//     amount100: 99123
+//   })
+//   .then(console.log)
+//   .catch(console.error);
