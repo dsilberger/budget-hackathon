aec995ab79767a09e26f2097f2c9b2b78bc4824d
Joe Yan <chengmu.yan@gmail.com>
2020-03-10 17:21:56 -0400

    Complete ExpensesModel.getAllExpenses()

--- server/models/expensesModel.js
+++ server/models/expensesModel.js
@@ -9,7 +9,7 @@ const model = {
     return db.queryAsync(q, [name]).then(({ insertId }) => model._getCategoryById(insertId));
   },
 
-  getCategories: () => {
+  getAllCategories: () => {
     const q = `
       select * from Categories`;
 
@@ -24,18 +24,23 @@ const model = {
     return db.queryAsync(q, [id]).then(dbRes => dbRes[0]);
   },
 
-  addExpense: ({ date, description, category: categoryName, accountName, amount100 }) => {
+  addExpense: ({ date, description, categoryId, accountName, amount100 }) => {
     const q = `
       insert into Expenses (date, description, categoryId, accountName, amount100)
       values (?, ?, ?, ?, ?)`;
 
-    return model
-      .getCategories()
-      .then(categories => {
-        let categoryId = categories.find(e => e.name === categoryName).id;
-        return categoryId ? categoryId : model.addCategory({ name: categoryName }).then(({ id }) => id);
-      })
-      .then(categoryId => db.queryAsync(q, [date, description, categoryId, accountName, amount100]));
+    return db
+      .queryAsync(q, [date, description, categoryId, accountName, amount100])
+      .then(({ insertId }) => model._getExpenseById(insertId));
+  },
+
+  getAllExpenses: () => {
+    const q = `
+      select e.date, e.description, c.name category, e.accountName, e.amount100
+      from Expenses e
+      left outer join Categories c on c.id = e.categoryId`;
+
+    return db.queryAsync(q);
   },
 
   _getExpenseById: id => {
@@ -48,23 +53,3 @@ const model = {
 };
 
 module.exports = model;
-
-// module.exports
-//   .addCategory({ name: "Test 2" })
-//   .then(dbRes => console.log(dbRes))
-//   .catch(err => {
-//     console.error(err);
-//   });
-
-// model
-//   .addExpense({
-//     date: "2020-02-02",
-//     description: "testing addExpense()",
-//     category: "Mortgage & Rent",
-//     accountName: "Test Acct",
-//     amount100: 99123
-//   })
-//   .then(console.log)
-//   .catch(console.error);
-
-model._getExpenseById(3).then(console.log);
