c5e554697bfbb59a5b97653eae2172ad1c4c31c6
Dan Silberger <dan.silberger@gmail.com>
2020-03-10 16:58:32 -0400

    Implement basic user CR[UD tbd]

---
+++ server/db/controllers/usersController.js
@@ -0,0 +1,36 @@
+const User = require("../../models/usersModel");
+
+module.exports.create = function(req, res) {
+  const userObj = req.body;
+
+  User.addUser(userObj)
+    .then(() => {
+      res.sendStatus(201);
+    })
+    .catch(err => {
+      console.error(`An error occured while creating a user: ${err}`);
+      res.sendStatus(500);
+    });
+};
+
+module.exports.retrieve = function(req, res) {
+  User.getUser()
+    .then(data => {
+      userData = data[0]["dataValues"];
+      // userData will be of the form
+      // {
+      //   id: 1,
+      //   name: 'Coffee Bear',
+      //   income: 1780000,
+      //   familySize: 3,
+      //   createdAt: 2020-03-10T20:51:48.000Z,
+      //   updatedAt: 2020-03-10T20:51:48.000Z
+      // }
+
+      res.send(userData);
+    })
+    .catch(err => {
+      console.error(`An error occured while creating a user: ${err}`);
+      res.sendStatus(500);
+    });
+};
