b1ff860d1c3a52a6225690ba1f8ca630fe1cbc05
Joe Yan <chengmu.yan@gmail.com>
2020-03-11 00:25:37 -0400

    Initialize ExpenseList component and provide expenseList prop via static state in App

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import ExpensesForm from "./components/ExpensesForm.jsx";
+import ExpenseList from "./components/ExpenseList.jsx";
 
 class App extends React.Component {
   constructor(props) {
@@ -23,6 +24,79 @@ class App extends React.Component {
           id: 3,
           name: "Mortgage & Rent"
         }
+      ],
+
+      expenseList: [
+        {
+          date: "2019-01-01T05:00:00.000Z",
+          description: "Skyba",
+          category: "Groceries",
+          accountName: "Credit Card 1",
+          amount100: 4900
+        },
+        {
+          date: "2019-01-01T05:00:00.000Z",
+          description: "Feedfish",
+          category: "Groceries",
+          accountName: "Credit Card 1",
+          amount100: 6080
+        },
+        {
+          date: "2019-01-01T05:00:00.000Z",
+          description: "Vinte",
+          category: "Restaurants",
+          accountName: "Credit Card 1",
+          amount100: 1012
+        },
+        {
+          date: "2019-01-01T05:00:00.000Z",
+          description: "Buzzshare",
+          category: "Restaurants",
+          accountName: "Credit Card 1",
+          amount100: 115000
+        },
+        {
+          date: "2019-01-01T05:00:00.000Z",
+          description: "Avaveo",
+          category: "Mortgage & Rent",
+          accountName: "Credit Card 1",
+          amount100: 30300
+        },
+        {
+          date: "2020-02-02T05:00:00.000Z",
+          description: "testing addExpense()",
+          category: "Groceries",
+          accountName: "Test Acct",
+          amount100: 99123
+        },
+        {
+          date: "2020-02-02T05:00:00.000Z",
+          description: "testing addExpense()",
+          category: "Groceries",
+          accountName: "Test Acct",
+          amount100: 99123
+        },
+        {
+          date: "2020-02-02T05:00:00.000Z",
+          description: "testing addExpense() 2",
+          category: "Groceries",
+          accountName: "Test Acct",
+          amount100: 99123
+        },
+        {
+          date: "2019-12-25T05:00:00.000Z",
+          description: "Postman post",
+          category: "Groceries",
+          accountName: "Credit Card 1",
+          amount100: 23222
+        },
+        {
+          date: "2019-12-25T05:00:00.000Z",
+          description: "Postman post",
+          category: "Groceries",
+          accountName: "Credit Card 1",
+          amount100: 23222
+        }
       ]
     };
   }
@@ -32,6 +106,7 @@ class App extends React.Component {
       <div>
         <h1>Bear Tracks Budgeting App!</h1>
         <ExpensesForm categories={this.state.categories} />
+        <ExpenseList expenseList={this.state.expenseList} />
       </div>
     );
   }
---
+++ client/src/components/ExpenseList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExpenseItem from "./ExpenseItem.jsx";
+
+const ExpenseList = ({ expenseList }) => {
+  return (
+    <div>
+      <h2>Expenses:</h2>
+    </div>
+  );
+};
+
+export default ExpenseList;
