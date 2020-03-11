2e4bde134d62ba15b11c36b079f3d4292453a6aa
Joe Yan <chengmu.yan@gmail.com>
2020-03-11 13:13:53 -0400

    Fix amount in ExpenseItem to render as amount/100

--- client/src/components/ExpenseItem.jsx
+++ client/src/components/ExpenseItem.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import moment from "moment";
 
-const ExpenseItem = ({ date, amount100: amount, category, description, accountName }) => {
+const ExpenseItem = ({ date, amount100, category, description, accountName }) => {
   return (
     <tr>
       <td>{new moment(date).format("YYYY-MM-DD")}</td>
-      <td>{amount}</td>
+      <td>{amount100 / 100}</td>
       <td>{category}</td>
       <td>{description}</td>
       <td>{accountName}</td>
