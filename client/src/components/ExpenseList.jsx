f88244eed7657f519524cbe74ed286a20734cb59
Joe Yan <chengmu.yan@gmail.com>
2020-03-11 00:42:13 -0400

    Format ExpenseList as table with ExpenseItem as table row; use moment library to format dates

--- client/src/components/ExpenseList.jsx
+++ client/src/components/ExpenseList.jsx
@@ -5,9 +5,22 @@ const ExpenseList = ({ expenseList }) => {
   return (
     <div>
       <h2>Expenses:</h2>
-      {expenseList.map(entry => (
-        <ExpenseItem key={entry.id} {...entry} />
-      ))}
+      <table>
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Amount ($)</th>
+            <th>Category</th>
+            <th>Description</th>
+            <th>Account</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenseList.map(entry => (
+            <ExpenseItem key={entry.id} {...entry} />
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
