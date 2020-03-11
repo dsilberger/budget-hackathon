0d8643601b9761aa2bc21d9cef3bd21b7bda3f64
Dan Silberger <dan.silberger@gmail.com>
2020-03-11 19:22:44 -0400

    pre-conditional logic rework commit

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import api from "../api.js";
+import "bulma";
 
 class UserForm extends React.Component {
   constructor(props) {
@@ -8,15 +9,30 @@ class UserForm extends React.Component {
     this.state = {
       username: "",
       income: "",
-      familySize: ""
+      familySize: "",
+      id: "",
+      editMode: false
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleClick = this.handleClick.bind(this);
+    this.updateProfile = this.updateProfile.bind(this);
   }
 
   handleChange(event) {
     this.setState({ [event.target.name]: event.target.value });
+  }
+
+  componentDidMount() {
+    api.fetchUserProfile().then(({ id, name, income, familySize }) => {
+      this.setState({
+        username: name,
+        id: id,
+        familySize: familySize,
+        income: income,
+        editMode: !this.state.editMode
+      });
+    });
   }
 
   handleClick(event) {
@@ -30,29 +46,88 @@ class UserForm extends React.Component {
     api.handleUserSubmit(newUserObject);
   }
 
+  updateProfile(event) {
+    event.preventDefault();
+
+    api.fetchUserProfile().then(({ id, name, income, familySize }) => {
+      this.setState({
+        username: name,
+        id: id,
+        familySize: familySize,
+        income: income,
+        editMode: !this.state.editMode
+      });
+    });
+
+    console.log(this.state);
+  }
+
   render() {
-    return (
-      <div>
-        <form>
-          <label>
-            Name:
-            <input type="text" name="username" onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Monthly Income:
-            <input type="text" name="income" onChange={this.handleChange} />
-          </label>
-          <br />
-          <label>
-            Family Size:
-            <input type="text" name="familySize" onChange={this.handleChange} />
-          </label>
-          <br />
-          <button onClick={this.handleClick}>Sign-up</button>
-        </form>
-      </div>
-    );
+    if (this.state.editMode) {
+      return (
+        <div>
+          <div classname="card">
+            <div class="card-header">
+              <div class="card-header-title">Your Profile</div>
+            </div>
+            <div class="card-content">
+              <ul>
+                <li>Name: {this.state.username}</li>
+                <li>Monthly Income: {this.state.income}</li>
+                <li>Family Size: {this.state.familySize}</li>
+              </ul>
+            </div>
+          </div>
+          <div classname="card-footer">
+            <div classname="card-footer-item">
+              <button className="button is-medium" onClick={this.updateProfile}>
+                Edit Profile
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <form>
+            <label>
+              Name:
+              <input
+                type="text"
+                value={this.state.username}
+                name="username"
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Monthly Income:
+              <input
+                type="text"
+                value={this.state.income}
+                name="income"
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <label>
+              Family Size:
+              <input
+                type="text"
+                value={this.state.familySize}
+                name="familySize"
+                onChange={this.handleChange}
+              />
+            </label>
+            <br />
+            <button className="button is-medium" onClick={this.updateProfile}>
+              Update
+            </button>
+          </form>
+        </div>
+      );
+    }
   }
 }
 
